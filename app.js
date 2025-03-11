0dac5fe9c73cde9953711e4ba3479bc8f1a68685
Lidia <hyoma.muroga.ds@gmail.com>
2025-03-11 16:27:31 -0300

    val-clr-add

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 //crea array
 let amigos = [];
+//funcion para agregar amigo
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;    
+    if(validarAmigo()){
+        amigos.push(amigo);    
+        console.log(amigos);
+        console.log(amigo);
+        limpiarText();
+        return; 
+    }
+}
+//valida que el campo no este vacio
+function validarAmigo(){ 
+    if (amigo.value.trim() == "") {
+        alert("Por favor, inserte un nombre");
+        return false;
+    }    
+    return true;    
+}
+//limpia el campo de texto
+function limpiarText(){
+    document.getElementById('amigo').value = "";
+}
