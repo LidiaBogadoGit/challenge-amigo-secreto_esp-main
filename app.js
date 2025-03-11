11c7c8210e5a4987a2b3d3f2018ebc08c7dce5b3
Lidia <hyoma.muroga.ds@gmail.com>
2025-03-11 18:28:34 -0300

    for-li

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-//crea array
+//array
 let amigos = [];
-//funcion para agregar amigo
+
+/*funcion para agregar amigo*/
 function agregarAmigo(){
     let amigo = document.getElementById('amigo').value;    
     if(validarAmigo()){
-        amigos.push(amigo);    
-        console.log(amigos);
-        console.log(amigo);
+        amigos.push(amigo);
         limpiarText();
         return; 
     }
+console.log(amigos);
+console.log(amigo);
 }
-//valida que el campo no este vacio
+
+/*valida que el campo no este vacio*/
 function validarAmigo(){ 
     if (amigo.value.trim() == "") {
         alert("Por favor, inserte un nombre");
@@ -19,7 +21,19 @@ function validarAmigo(){
     }    
     return true;    
 }
-//limpia el campo de texto
+
+/*limpia el campo de texto*/
 function limpiarText(){
     document.getElementById('amigo').value = "";
+}
+
+/*funcion para agregar amigos a una lista html*/
+function listaHTML() {
+    let lista = document.getElementById('listaAmigos');
+    // Limpiar la lista
+    lista.innerHTML = ""; 
+    // Recorrer el array de amigos
+    amigos.forEach(function(amigo) {
+        lista.innerHTML += `<l>${nombre}</li>`;
+    });
 }
